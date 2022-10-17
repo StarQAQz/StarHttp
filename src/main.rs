a8aeb9f119cffac7e7726f29af85bdad4a4c2c13
zhengzt21 <820813748@qq.com>
2022-10-17 17:35:44 +0800

    修改为多线程

--- src/main.rs
+++ src/main.rs
@@ -9,144 +9,143 @@ use std::{
     io::{self, BufRead, BufReader, Read, Write},
     net::{Ipv4Addr, SocketAddrV4, TcpListener, TcpStream},
     path::Path,
+    thread,
 };
 
 const STATIC_RESOURCE_PATH: &str = "./static";
 
-struct HttpConnect {
-    tcp: TcpStream,
-}
-
-impl HttpConnect {
-    //连接控制，读取请求并判断请求类型
-    fn handle_connect(self) {
-        if let Ok(first_line) = self.read_line() {
-            if let Some(first_line) = first_line {
-                println!("{}", &first_line);
-                let mut header = first_line.split_whitespace();
-                let request_type = header.next().unwrap();
-                let url = header.next().unwrap();
-                //分发请求类型处理
-                match request_type.to_lowercase().as_str() {
-                    "get" => {
-                        if let Err(e) = self.get(url) {
-                            eprintln!(
-                                "The GET request is abnormal. Error reason: {}",
-                                e.to_string()
-                            );
-                            if let Err(e) = self.send_internal_server_error() {
-                                eprintln!("Response 500 failed. Error reason: {}", e.to_string());
-                            }
+//连接控制，读取请求并判断请求类型
+fn handle_connect(stream: TcpStream) {
+    if let Ok(first_line) = read_line(&stream) {
+        if let Some(first_line) = first_line {
+            println!("{}", &first_line);
+            let mut header = first_line.split_whitespace();
+            let request_type = header.next().unwrap();
+            let url = header.next().unwrap();
+            //分发请求类型处理
+            match request_type.to_lowercase().as_str() {
+                "get" => {
+                    if let Err(e) = get(&stream, url) {
+                        eprintln!(
+                            "The GET request is abnormal. Error reason: {}",
+                            e.to_string()
+                        );
+                        if let Err(e) = send_internal_server_error(&stream) {
+                            eprintln!("Response 500 failed. Error reason: {}", e.to_string());
                         }
                     }
-                    val => {
-                        eprintln!("Do not support request type! Request type: {}", val);
-                    }
-                };
-            }
-        }
-        self.shutdown();
-    }
-
-    //GET请求
-    fn get(&self, url: &str) -> io::Result<()> {
-        //读取剩余请求
-        while let Some(_) = self.read_line()? {}
-        //解析url，分隔参数
-        let mut path = url;
-        if url.contains("?") {
-            let v: Vec<&str> = url.split("?").collect();
-            path = v[0];
-        }
-        //构建文件路径
-        let mut current_path = fs::canonicalize(Path::new(STATIC_RESOURCE_PATH))?;
-        for node in path.split("/") {
-            current_path.push(node);
-        }
-        match File::open(current_path) {
-            Ok(file) => self.send_ok(file)?,
-            Err(_) => self.send_not_found()?,
-        }
-        Ok(())
-    }
-
-    fn send_ok(&self, file: File) -> io::Result<()> {
-        let mut params: HashMap<&str, &str> = HashMap::new();
-        params.insert("Content-Type", "text/html;text/html; charset=utf-8\r\n");
-        let header = ResponseHeader {
-            http_status: HttpStatus::OK,
-            params,
-        };
-        self.send(header, Some(&file))
-    }
-
-    fn send_not_found(&self) -> io::Result<()> {
-        let mut params: HashMap<&str, &str> = HashMap::new();
-        params.insert("Content-Type", "text/html;text/html; charset=utf-8\r\n");
-        let header = ResponseHeader {
-            http_status: HttpStatus::NotFound,
-            params,
-        };
-        let html = "<!DOCTYPE html><head><title>404 NOT FOUND</title></head><body><h1>404 NOT FOUND!</h1></body></html>";
-        self.send(header, Some(&html.to_string()))
-    }
-
-    fn send_internal_server_error(&self) -> io::Result<()> {
-        let mut params: HashMap<&str, &str> = HashMap::new();
-        params.insert("Content-Type", "text/html;text/html; charset=utf-8\r\n");
-        let header = ResponseHeader {
-            http_status: HttpStatus::InternalServerError,
-            params,
-        };
-        let html = "<!DOCTYPE html><head><title>500 INTERNAL SERVER ERROR</title></head><body><h1>500 INTERNAL SERVER ERROR!</h1></body></html>";
-        self.send(header, Some(&html.to_string()))
-    }
-
-    fn send(&self, header: ResponseHeader, body: Option<&dyn ResponseBody>) -> io::Result<()> {
-        let mut tcp = &self.tcp;
-        tcp.write_all(header.get().as_bytes())?;
-        if let Some(body) = body {
-            body.write_in_connect(self)?;
-        }
-        Ok(())
-    }
-
-    fn shutdown(self) {
-        if let Err(e) = self.tcp.shutdown(std::net::Shutdown::Both) {
-            eprintln!(
-                "Failed to shutdown the connection. Error reason: {}",
-                e.to_string()
-            );
-        }
-    }
-
-    /*
-     * 读取一行数据
-     */
-    fn read_line(&self) -> io::Result<Option<String>> {
-        let mut line = String::new();
-        let tcp = &self.tcp;
-        for buf in tcp.bytes() {
-            let val = buf? as char;
-            if val == '\r' {
-                let val = tcp.bytes().next().unwrap();
-                if val? as char != '\n' {
-                    return Err(io::Error::new(
-                        io::ErrorKind::Other,
-                        "read line error: Read only '\\r', no '\\n'",
-                    ));
-                } else {
-                    break;
                 }
+                val => {
+                    eprintln!("Do not support request type! Request type: {}", val);
+                }
+            };
+        }
+    }
+    shutdown(stream);
+}
+
+//GET请求
+fn get(stream: &TcpStream, url: &str) -> io::Result<()> {
+    //读取剩余请求
+    while let Some(_) = read_line(&stream)? {}
+    //解析url，分隔参数
+    let mut path = url;
+    if url.contains("?") {
+        let v: Vec<&str> = url.split("?").collect();
+        path = v[0];
+    }
+    //构建文件路径
+    let mut current_path = fs::canonicalize(Path::new(STATIC_RESOURCE_PATH))?;
+    for node in path.split("/") {
+        current_path.push(node);
+    }
+    match File::open(current_path) {
+        Ok(file) => send_ok(stream, file)?,
+        Err(_) => send_not_found(stream)?,
+    }
+    Ok(())
+}
+
+fn send_ok(stream: &TcpStream, file: File) -> io::Result<()> {
+    let mut params: HashMap<&str, &str> = HashMap::new();
+    params.insert("Content-Type", "text/html;text/html; charset=utf-8\r\n");
+    let header = ResponseHeader {
+        http_status: HttpStatus::OK,
+        params,
+    };
+    send(stream, header, Some(&file))
+}
+
+fn send_not_found(stream: &TcpStream) -> io::Result<()> {
+    let mut params: HashMap<&str, &str> = HashMap::new();
+    params.insert("Content-Type", "text/html;text/html; charset=utf-8\r\n");
+    let header = ResponseHeader {
+        http_status: HttpStatus::NotFound,
+        params,
+    };
+    let html = "<!DOCTYPE html><head><title>404 NOT FOUND</title></head><body><h1>404 NOT FOUND!</h1></body></html>";
+    send(stream, header, Some(&html.to_string()))
+}
+
+fn send_internal_server_error(stream: &TcpStream) -> io::Result<()> {
+    let mut params: HashMap<&str, &str> = HashMap::new();
+    params.insert("Content-Type", "text/html;text/html; charset=utf-8\r\n");
+    let header = ResponseHeader {
+        http_status: HttpStatus::InternalServerError,
+        params,
+    };
+    let html = "<!DOCTYPE html><head><title>500 INTERNAL SERVER ERROR</title></head><body><h1>500 INTERNAL SERVER ERROR!</h1></body></html>";
+    send(stream, header, Some(&html.to_string()))
+}
+
+fn send(
+    stream: &TcpStream,
+    header: ResponseHeader,
+    body: Option<&dyn ResponseBody>,
+) -> io::Result<()> {
+    let mut tcp = stream;
+    tcp.write_all(header.get().as_bytes())?;
+    if let Some(body) = body {
+        body.write_in_connect(stream)?;
+    }
+    Ok(())
+}
+
+fn shutdown(stream: TcpStream) {
+    if let Err(e) = stream.shutdown(std::net::Shutdown::Both) {
+        eprintln!(
+            "Failed to shutdown the connection. Error reason: {}",
+            e.to_string()
+        );
+    }
+}
+
+/*
+ * 读取一行数据
+ */
+fn read_line(stream: &TcpStream) -> io::Result<Option<String>> {
+    let mut line = String::new();
+    let tcp = stream;
+    for buf in tcp.bytes() {
+        let val = buf? as char;
+        if val == '\r' {
+            let val = tcp.bytes().next().unwrap();
+            if val? as char != '\n' {
+                return Err(io::Error::new(
+                    io::ErrorKind::Other,
+                    "read line error: Read only '\\r', no '\\n'",
+                ));
             } else {
-                line.push(val);
+                break;
             }
-        }
-        if line.len() > 0 {
-            Ok(Some(line))
         } else {
-            Ok(None)
+            line.push(val);
         }
+    }
+    if line.len() > 0 {
+        Ok(Some(line))
+    } else {
+        Ok(None)
     }
 }
 
@@ -168,12 +167,12 @@ impl ResponseHeader<'_> {
 }
 
 trait ResponseBody {
-    fn write_in_connect(&self, connect: &HttpConnect) -> io::Result<()>;
+    fn write_in_connect(&self, stream: &TcpStream) -> io::Result<()>;
 }
 
 impl ResponseBody for File {
-    fn write_in_connect(&self, connect: &HttpConnect) -> io::Result<()> {
-        let mut tcp = &connect.tcp;
+    fn write_in_connect(&self, stream: &TcpStream) -> io::Result<()> {
+        let mut tcp = stream;
         let mut buf_reader = BufReader::new(self);
         while buf_reader.fill_buf()?.len() > 0 {
             let size = tcp.write(buf_reader.buffer())?;
@@ -185,8 +184,8 @@ impl ResponseBody for File {
 }
 
 impl ResponseBody for String {
-    fn write_in_connect(&self, connect: &HttpConnect) -> io::Result<()> {
-        let mut tcp = &connect.tcp;
+    fn write_in_connect(&self, stream: &TcpStream) -> io::Result<()> {
+        let mut tcp = stream;
         tcp.write_all(self.as_bytes())?;
         tcp.flush()?;
         Ok(())
@@ -223,17 +222,37 @@ fn init_check() {
     }
 }
 
+pub struct ThreadPool {
+    threads: Vec<thread::JoinHandle<()>>,
+}
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+        let mut threads: Vec<thread::JoinHandle<()>> = Vec::with_capacity(size);
+        for _i in 0..size {}
+
+        ThreadPool { threads }
+    }
+    pub fn exec<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+    }
+}
+
 fn main() {
     init().expect("Initialization failed");
     init_check();
     let socket_addr = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 80);
     let listener = TcpListener::bind(socket_addr).unwrap();
 
+    let pool = ThreadPool::new(5);
+
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                let http_connect = HttpConnect { tcp: stream };
-                http_connect.handle_connect();
+                pool.exec(|| handle_connect(stream));
             }
             Err(e) => {
                 eprintln!("Connect Incoming Error:{}", e)
